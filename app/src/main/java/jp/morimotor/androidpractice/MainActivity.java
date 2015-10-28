52942da24e541d277cbf1748a00b8e6441bb50fb
morimotor <emuryu@gmail.com>
2015-10-29 03:39:18 +0900

    add init

--- app/src/main/java/jp/morimotor/androidpractice/MainActivity.java
+++ app/src/main/java/jp/morimotor/androidpractice/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -16,8 +15,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ((Button)findViewById(R.id.button2)).setOnClickListener(this);
-
+        findViewById(R.id.button).setOnClickListener(this);
+        findViewById(R.id.button2).setOnClickListener(this);
+        findViewById(R.id.button3).setOnClickListener(this);
+        findViewById(R.id.button4).setOnClickListener(this);
 
 
 
@@ -28,8 +29,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         Intent intent;
         switch (v.getId()){
+            case R.id.button:
+                intent = new Intent(this, CameraActivity.class);
+                startActivity(intent);
+                break;
+
             case R.id.button2:
                 intent = new Intent(this, AccelerationActivity.class);
+                startActivity(intent);
+                break;
+
+            case R.id.button3:
+                intent = new Intent(this, ManpokeiActivity.class);
+                startActivity(intent);
+                break;
+
+            case R.id.button4:
+                intent = new Intent(this, GoogleFitActivity.class);
                 startActivity(intent);
                 break;
         }
